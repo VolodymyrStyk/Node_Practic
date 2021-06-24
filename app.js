a544872eb4fb962d654e3793246f93a51a064ebc
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-24 23:29:10 +0300

    init commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const fs = require('fs/promises');
 const express = require('express');
+const fs = require('fs/promises');
 const path = require('path');
 
 const app = express();
+const userPath = path.join(__dirname,'users.json');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
